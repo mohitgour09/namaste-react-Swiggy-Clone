1ca3a42cfd7daf10f9c4d1d7bcd97300862da892
mohitgour09 <mohitgour0009@gmail.com>
2025-03-19 15:35:50 +0530

    igniting the App

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am H1 tag"),
+    React.createElement("h1", {}, "Namaste react "),
     React.createElement("h2", {}, "I am H2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
